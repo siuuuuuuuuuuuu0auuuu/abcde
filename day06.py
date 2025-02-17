f72c623d476eaad749ec96e96f11189f01b80156
ãLEE <junecjsk@naver.com>
2025-02-17 17:48:34 +0900

    v1.5day06 assignment

--- day06.py
+++ day06.py
@@ -1,5 +1,34 @@
-n= int(input())
-r= 0
-for i in range(n+1):
-    r= r + i
-print(r)
+# assignment day06
+
+import random
+
+def guess_number(low, high, answer, chance) -> int:
+    mid =  (low+high) // 2
+    print(f'Guess number is {mid}')
+    fp.write(f'Guess number is {mid}\n')
+    while chance != 0:
+        if mid == answer:
+            print(f'You win. Answer is {answer}')
+            fp.write(f'You win. Answer is {answer}\n')
+            return
+        elif mid > answer:
+            chance = chance - 1
+            print(f'{mid} is bigger. Chance left : {chance}')
+            fp.write(f'{mid} is bigger. Chance left : {chance}\n')
+            return guess_number(low, mid-1, answer, chance)
+        else:
+            chance = chance - 1
+            print(f'{mid} is lower. Chance left : {chance}')
+            fp.write(f'{mid} is lower. Chance left : {chance}\n')
+            return guess_number(mid+1, high, answer, chance)
+    else:
+        print(f'You lost. Answer is {answer}')
+        fp.write(f'You lost. Answer is {answer}')
+
+if __name__ == "__main__":
+    low = 1
+    high = 100
+    chance = 7
+    answer = random.randint(low, high)
+    with open('guess.txt', 'w') as fp:
+        guess_number(low, high, answer, chance)
